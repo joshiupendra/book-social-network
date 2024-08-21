77fcdb26cb6b73a26b7105b53f8b8894ba7517be
Upendra Joshi <joshiupendra9@gmail.com>
2024-08-21 22:40:35 +0530

    book-network-api

---
+++ book-network/src/main/java/com/seemay/book/auth/AuthenticationService.java
@@ -0,0 +1,39 @@
+package com.seemay.book.auth;
+
+import com.seemay.book.role.RoleRepository;
+import com.seemay.book.user.User;
+import com.seemay.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                // todo - better exception handling
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+    }
+}
