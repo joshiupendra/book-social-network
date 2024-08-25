a7df36dfe23f14cc5d2b40c4a9489545f5272e15
Upendra Joshi <joshiupendra9@gmail.com>
2024-08-25 19:05:08 +0530

    Implemented Login functionality

---
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationRequest, AuthenticationResponse } from 'src/app/services/models';
+import { AuthenticationService } from 'src/app/services/services';
+import { TokenService } from 'src/app/services/token/token.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  authRequest: AuthenticationRequest = { email: "", password: "" };
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: TokenService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  login(): void {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res:AuthenticationResponse): void => {
+        console.log(res);
+        this.tokenService.token = res.token as string;
+        this.router.navigate(["books"]);
+      },
+      error: (err): void => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.error);
+        }
+      }
+    });
+  }
+
+  register(): void {
+    this.router.navigate(["register"]);
+  }
+}
